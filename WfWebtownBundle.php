448931c96136e9e6a0f52cc3fb7510cae248df0a
Krisztián Ferenczi <ferenczi.krisztian@gmail.com>
2019-08-03 07:54:21 +0200

    Bundle fix

--- WfWebtownBundle.php
+++ WfWebtownBundle.php
@@ -4,10 +4,18 @@ namespace Wf\Webtown;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Wf\DockerWorkflowBundle\Recipes\BaseRecipe;
+use Wf\DockerWorkflowBundle\WfDockerWorkflowBundle;
+use Wf\DockerWorkflowBundle\Wizard\WizardInterface;
 
 class WfWebtownBundle extends Bundle
 {
     public function build(ContainerBuilder $container)
     {
+        // Register autoconfigurations
+        $container->registerForAutoconfiguration(BaseRecipe::class)
+            ->addTag(WfDockerWorkflowBundle::RECIPE_TAG);
+        $container->registerForAutoconfiguration(WizardInterface::class)
+            ->addTag(WfDockerWorkflowBundle::WIZARD_TAG);
     }
 }
